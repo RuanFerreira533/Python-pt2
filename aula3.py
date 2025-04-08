4626dd40774d387458b9c5f40effd1fc240bff4a
Ruan Ferreira <ruanferreira533@gmail.com>
2025-04-08 18:19:25 -0300

    Create aula3.py

---
+++ aula3.py
@@ -0,0 +1,16 @@
+# Utilizando comentários em Python #
+'''
+Novo tipo de compentario usando 3 aspas
+'''
+
+nome  = input("Qual o seu nome?\n")
+idade = int( input("Qual sua idade?\n") )
+salario = float( input("Quanto você recebe?\n") )
+sexo  = input("Você já se cadastrou no nosso site?\n " ) 
+
+
+print(nome)
+print(idade)
+print(salario)
+print(sexo)
+ 
